ef163450048d33361b0deb0b5d2dec1b15988648
Antônio Evaldo <antonio.evaldo349@gmail.com>
2023-01-23 16:43:06 -0300

    tratando ValidationError

---
+++ src/erros/ErroValidacao.js
@@ -0,0 +1,13 @@
+import ErroBase from "./ErroBase.js";
+
+class ErroValidacao extends ErroBase {
+  constructor(erros) {
+    const mensagensErro = Object.values(erros)
+      .map((erro) => erro.message)
+      .join("\n");
+
+    super(`Os seguintes erros foram encontrados: ${mensagensErro}`, 400);
+  }
+}
+
+export default ErroValidacao;
