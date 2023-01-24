fc078993601e34419ff997a2c483427fa7e3f7fc
Antônio Evaldo <antonio.evaldo349@gmail.com>
2023-01-24 15:59:49 -0300

    validando CastError de campo de uma requisição

--- src/erros/ErroValidacao.js
+++ src/erros/ErroValidacao.js
@@ -1,9 +1,16 @@
+import mongoose from "mongoose";
 import ErroBase from "./ErroBase.js";
 
 class ErroValidacao extends ErroBase {
   constructor(erros = []) {
     const mensagensErro = Object.values(erros)
-      .map((erro) => erro.message)
+      .map((erro) => {
+        if (erro instanceof mongoose.Error.CastError) {
+          return `Dado "${erro.path}" inválido.`
+        }
+        
+        return erro.message;
+      })
       .join("; ");
 
     super(`Os seguintes erros foram encontrados: ${mensagensErro}`, 400);
